c1308ebb97f29429935af25a6721183319d30e10
Walid Imam <wimam@e3r6p5.1337.ma>
2026-07-20 02:46:49 +0100

    ex02

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -10,11 +10,26 @@
 //class
 class PmergeMe
 {
+/** private member variables **/
 	private:
 		std::vector<std::pair<int, int> >	_elements;
+		std::vector<int>					_chain;
+		std::vector<int>					_pend;
 		bool								_hasStraggler;
 		int									_straggler;
 
+/** private member functions **/
+	private:
+	//sort
+		void	binaryInsert(int value);
+		void	insertPendElements(std::vector<size_t> order);
+		void	buildMainChain(void);
+		void	merge(size_t left, size_t mid, size_t right);
+		void	mergeSort(size_t left, size_t right);
+		void	swap_sortPairs(void);
+		void	sort(void);
+
+/** public member functions **/
 	public:
 	//orthodox;
 		PmergeMe();
@@ -24,7 +39,6 @@ class PmergeMe
 
 	//methods
 		void	printElements(void) const;
-		void	sort(void);
 		void	init(int ac, char** av);
 		void	lunch(void);
 	

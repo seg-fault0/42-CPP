eb1d0e35e77f701d96d4182ba1beb97b67b9cb5c
Walid Imam <wimam@e3r5p5.1337.ma>
2026-08-16 18:07:54 +0100

    cpp09/ex02 : fix time

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -281,7 +281,7 @@ void PmergeMe<PairContainer, IntContainer>::lunch()
 
 	std::clock_t end = std::clock();
 
-	_time = static_cast<double>(end - start) / 1000;
+	_time = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000000;
 }
 
 /******************************************/
